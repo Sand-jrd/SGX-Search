a8cc7f1f49204f2d9847fc84063aebaa5d045eaf
Sandrine Juillard <s.juillard@hotmail.fr>
2020-11-24 07:11:51 +0100

    initial commit

--- Enclave.cpp
+++ Enclave.cpp
@@ -5,38 +5,60 @@
 #include <list>
 #include <stdlib.h> 
 #include <cstdlib>
+#include <iterator>
 #include <iostream>
-#include <winsock2.h>
-#include <windows.h>
-#include <locale>
 #include <sstream>
+#include <algorithm>
+
+
+
 using namespace std;
 
 
 #define MAXSIZE  1024;
 #define HISTSIZE 1024;
 
-HINSTANCE hInst;
-WSADATA wsaData;
-void mParseUrl(char *mUrl, string &serverName, string &filepath, string &filename);
-SOCKET connectToServer(char *szServerName, WORD portNum);
-int getHeaderLength(char *content);
-char *readUrl2(char *szUrl, long &bytesReturnedOut, char **headerOut);
 
-/* Generate an obfuscated Query
-INPUT  Q  : Initial Query
-		H  : History of Query
-		k  : Numbers of fake query
-OUTPUT  OQ : obfuscatedQuery
+vector <string> seperateWords(string S) {
+	std::stringstream ss(S);
+	std::istream_iterator<std::string> begin(ss);
+	std::istream_iterator<std::string> end;
+	std::vector<std::string> vstrings(begin, end);
+	return vstrings;
+}
+
+
+int countCommon(string  Qi, string  Ri) {
+
+	vector <string> QWrords = seperateWords(Qi);
+	vector <string> RWrords = seperateWords(Ri);
+	int score = 0;
+
+	for (std::vector<string>::const_iterator Qwi = QWrords.begin(); Qwi != QWrords.end(); ++Qwi)
+		for (std::vector<string>::const_iterator Rwi = RWrords.begin(); Rwi != RWrords.end(); ++Rwi) {
+			if (*Qwi == *Rwi)
+				score++;
+		}
+
+
+	return score;
+}
+
+
+// Generate an obfuscated Query
+/* INPUT  Q  : Initial Query
+		  H  : History of Query
+		  k  : Numbers of fake query
+   OUTPUT OQ : obfuscatedQuery
 */
 void obfuscated(string  Q, vector <string> H, int k, vector <string> * OQ) {
 
 	int OQ_size = 0;
-	int id = rand() % k+1;
+	int id = rand() % k + 1;
 
 	while (OQ_size <= k)
 	{
-		if (id == 0){
+		if (id == 0) {
 			OQ->push_back(Q);
 		}
 
@@ -51,202 +73,67 @@ void obfuscated(string  Q, vector <string> H, int k, vector <string> * OQ) {
 
 }
 
-int sendQuery() {
 
-	const int bufLen = 1024;
-	char *szUrl = "http://stackoverflow.com";
-	long fileSize;
-	char *memBuffer, *headerBuffer;
-	FILE *fp;
-
-	memBuffer = headerBuffer = NULL;
-
-	if (WSAStartup(0x101, &wsaData) != 0)
-		return -1;
+// Fitrate Answere
+/*INPUT  Q  : Initial Query
+		 H  : History of Query
+		 R  : Set of result
+  OUTPUT ER : Estimatre result to the initial query
+*/
+void filtering(string  Q, vector <string> HQ, vector <string> R, vector <string> * ER) {
 
 
-	memBuffer = readUrl2(szUrl, fileSize, &headerBuffer);
-	printf("returned from readUrl\n");
-	printf("data returned:\n%s", memBuffer);
-	if (fileSize != 0)
-	{
-		printf("Got some data\n");
-		fp = fopen("downloaded.file", "wb");
-		fwrite(memBuffer, 1, fileSize, fp);
-		fclose(fp);
-		delete(memBuffer);
-		delete(headerBuffer);
-	}
+	int OQ_size = 0;
 
-	WSACleanup();
-	return 0;
-}
+	HQ.insert(HQ.begin(), Q);   // Real query will be the first element 
+	vector <int> scores;
 
+	for (std::vector<string>::const_iterator Ri = R.begin(); Ri != R.end(); ++Ri) {
 
-void mParseUrl(char *mUrl, string &serverName, string &filepath, string &filename)
-{
-	string::size_type n;
-	string url = mUrl;
+		scores.clear();
 
-	if (url.substr(0, 7) == "http://")
-		url.erase(0, 7);
-
-	if (url.substr(0, 8) == "https://")
-		url.erase(0, 8);
-
-	n = url.find('/');
-	if (n != string::npos)
-	{
-		serverName = url.substr(0, n);
-		filepath = url.substr(n);
-		n = filepath.rfind('/');
-		filename = filepath.substr(n + 1);
-	}
-
-	else
-	{
-		serverName = url;
-		filepath = "/";
-		filename = "";
-	}
-}
-
-SOCKET connectToServer(char *szServerName, WORD portNum)
-{
-	struct hostent *hp;
-	unsigned int addr;
-	struct sockaddr_in server;
-	SOCKET conn;
-
-	conn = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (conn == INVALID_SOCKET)
-		return NULL;
-
-	if (inet_addr(szServerName) == INADDR_NONE)
-	{
-		hp = gethostbyname(szServerName);
-	}
-	else
-	{
-		addr = inet_addr(szServerName);
-		hp = gethostbyaddr((char*)&addr, sizeof(addr), AF_INET);
-	}
-
-	if (hp == NULL)
-	{
-		closesocket(conn);
-		return NULL;
-	}
-
-	server.sin_addr.s_addr = *((unsigned long*)hp->h_addr);
-	server.sin_family = AF_INET;
-	server.sin_port = htons(portNum);
-	if (connect(conn, (struct sockaddr*)&server, sizeof(server)))
-	{
-		closesocket(conn);
-		return NULL;
-	}
-	return conn;
-}
-
-int getHeaderLength(char *content)
-{
-	const char *srchStr1 = "\r\n\r\n", *srchStr2 = "\n\r\n\r";
-	char *findPos;
-	int ofset = -1;
-
-	findPos = strstr(content, srchStr1);
-	if (findPos != NULL)
-	{
-		ofset = findPos - content;
-		ofset += strlen(srchStr1);
-	}
-
-	else
-	{
-		findPos = strstr(content, srchStr2);
-		if (findPos != NULL)
-		{
-			ofset = findPos - content;
-			ofset += strlen(srchStr2);
+		for (std::vector<string>::const_iterator Qi = HQ.begin(); Qi != HQ.end(); ++Qi) {
+			scores.push_back(countCommon(*Qi, *Ri));
 		}
-	}
-	return ofset;
-}
 
-char *readUrl2(char *szUrl, long &bytesReturnedOut, char **headerOut)
-{
-	const int bufSize = 512;
-	char readBuffer[bufSize], sendBuffer[bufSize], tmpBuffer[bufSize];
-	char *tmpResult = NULL, *result;
-	SOCKET conn;
-	string server, filepath, filename;
-	long totalBytesRead, thisReadSize, headerLen;
 
-	mParseUrl(szUrl, server, filepath, filename);
+		if (scores.at(0) == *max_element(std::begin(scores), std::end(scores))) {
+			ER->push_back(*Ri);
+		}
 
-	///////////// step 1, connect //////////////////////
-	conn = connectToServer((char*)server.c_str(), 80);
-
-	///////////// step 2, send GET request /////////////
-	sprintf(tmpBuffer, "GET %s HTTP/1.0", filepath.c_str());
-	strcpy(sendBuffer, tmpBuffer);
-	strcat(sendBuffer, "\r\n");
-	sprintf(tmpBuffer, "Host: %s", server.c_str());
-	strcat(sendBuffer, tmpBuffer);
-	strcat(sendBuffer, "\r\n");
-	strcat(sendBuffer, "\r\n");
-	send(conn, sendBuffer, strlen(sendBuffer), 0);
-
-	//    SetWindowText(edit3Hwnd, sendBuffer);
-	printf("Buffer being sent:\n%s", sendBuffer);
-
-	///////////// step 3 - get received bytes ////////////////
-	// Receive until the peer closes the connection
-	totalBytesRead = 0;
-	while (1)
-	{
-		memset(readBuffer, 0, bufSize);
-		thisReadSize = recv(conn, readBuffer, bufSize, 0);
-
-		if (thisReadSize <= 0)
-			break;
-
-		tmpResult = (char*)realloc(tmpResult, thisReadSize + totalBytesRead);
-
-		memcpy(tmpResult + totalBytesRead, readBuffer, thisReadSize);
-		totalBytesRead += thisReadSize;
 	}
 
-	headerLen = getHeaderLength(tmpResult);
-	long contenLen = totalBytesRead - headerLen;
-	result = new char[contenLen + 1];
-	memcpy(result, tmpResult + headerLen, contenLen);
-	result[contenLen] = 0x0;
-	char *myTmp;
 
-	myTmp = new char[headerLen + 1];
-	strncpy(myTmp, tmpResult, headerLen);
-	myTmp[headerLen] = NULL;
-	delete(tmpResult);
-	*headerOut = myTmp;
-
-	bytesReturnedOut = contenLen;
-	closesocket(conn);
-	return(result);
 }
+
+
+
 
 int main() {
 
-	string Q = "coucou";
-	vector <string> H = {"first","sec","third","fourth"};
+
+	// Test Obfucated 
+	cout << "\nStart Obfucated" << endl;
+
+	string Q = "Comment dire Coucou";
+	vector <string> H = { "il faut dire first","ou peux être sec"," 1 2 3 third","// é pm fourth" };
 	vector <string> OQ;
 
 	obfuscated(Q, H, 3, &OQ);
 
-
 	for (std::vector<string>::const_iterator i = OQ.begin(); i != OQ.end(); ++i)
-		std::cout << *i << ' ';
+		std::cout << *i << " OR ";
+
+	// Test Filtering
+	cout << "\n\nStart Filtering" << endl;
+
+	vector <string> R = { "first of all"," let me one sec","third alors","bon bon fourth","les coucou amies" };
+	vector <string> ER;
+
+	filtering(Q, H, R, &ER);
+
+	for (std::vector<string>::const_iterator i = ER.begin(); i != ER.end(); ++i)
+		std::cout << *i << " \n ";
 
 	return 1;
 }
