21412acbe23d62173f6bc1dae92e3e65e931ce95
Ubuntu <azureuser@LinuxSGX.pyvhczr4y5cujinc1aqn0nrvha.jx.internal.cloudapp.net>
2020-11-29 18:14:27 +0000

    requete google

--- SendRequest.cpp
+++ SendRequest.cpp
@@ -1,98 +1,62 @@
-
+#include <iostream>
+#include <ctype.h>
+#include <cstring>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
 #include <netdb.h>
-#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <unistd.h>
-#define INVALID_SOCKET -1
-#define SOCKET_ERROR -1
-#define closesocket(s) close(s)
-typedef int SOCKET;
-typedef struct sockaddr_in SOCKADDR_IN;
-typedef struct sockaddr SOCKADDR;
+#include <sstream>
+#include <fstream>
+#include <string>
 
-#include <stdio.h>
-#include <stdlib.h>
-#define PORT 8080
+using namespace std;
 
-int main(void)
+int sock;
+struct sockaddr_in client;
+int PORT = 80;
+
+int main(int argc, char const *argv[])
 {
-	int test = 0;
-	int ok = 0;
+    struct hostent * host = gethostbyname("google.com");
 
-	SOCKET sock;
-	SOCKADDR_IN sin;
+    if ( (host == NULL) || (host->h_addr == NULL) ) {
+        cout << "Error retrieving DNS information." << endl;
+        exit(1);
+    }
 
-	char reception[9999] = "";
+    bzero(&client, sizeof(client));
+    client.sin_family = AF_INET;
+    client.sin_port = htons( PORT );
+    memcpy(&client.sin_addr, host->h_addr, host->h_length);
 
-	struct hostent *HostInfo;
-	HostInfo = gethostbyname("www.aerofrance-fs.fr");
-	char *szLocalIP;
-	szLocalIP = inet_ntoa(*(struct in_addr *) *HostInfo->h_addr_list);
+    sock = socket(AF_INET, SOCK_STREAM, 0);
 
-	if (!erreur)
-	{
-		/* Création de la socket */
-		sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        cout << "Error creating socket." << endl;
+        exit(1);
+    }
 
-		/* Configuration de la connexion */
-		sin.sin_addr.s_addr = inet_addr(szLocalIP);
-		sin.sin_family = AF_INET;
-		sin.sin_port = htons(PORT);
+    if ( connect(sock, (struct sockaddr *)&client, sizeof(client)) < 0 ) {
+        close(sock);
+        cout << "Could not connect" << endl;
+        exit(1);
+    }
 
-		do
-		{
-			test++;
-			/* Si le client arrive à se connecter */
-			if (connect(sock, (SOCKADDR *)& sin, sizeof(sin)) !=
-				SOCKET_ERROR)
-			{
-				int ret;
-				send(sock,
-					"GET /index.php HTTP/1.1\r\nhost: www.aerofrance-fs.fr\r\n\r\n",
-					sizeof
-					("GET /index.php HTTP/1.1\r\nhost: www.aerofrance-fs.fr\r\n\r\n"),
-					0);
-				printf("Connexion à %s sur le port %d\n",
-					inet_ntoa(sin.sin_addr), htons(sin.sin_port));
-				ok = 1;
+    stringstream ss;
+    ss << "GET /search?q=a+b" << "HTTP/1.1\r\n" << "\r\n\r\n";
+    string request = ss.str();
 
+    if (send(sock, request.c_str(), request.length(), 0) != (int)request.length()) {
+        cout << "Error sending request." << endl;
+        exit(1);
+    }
 
-				while ((ret = recv(sock, reception, 9999, 0)))
-				{
-					reception[ret] = '\0';
-					printf("%s", reception);
-				}
+    char cur;
+    while ( read(sock, &cur, 1) > 0 ) {
+        cout << cur;
+    }
 
-				shutdown(sock, 2);
-			}
-			else
-				printf("Tentative de connexion %d...\n", test);
-		} while (test < 10 && ok == 0);
-
-
-
-		/* On ferme la socket précédemment ouverte */
-		closesocket(sock);
-
-	}
-
-
-	return EXIT_SUCCESS;
+    return 0;
 }
-
-/*
-int main()
-{
-	const ushort dim_buff_recv = 1024; // taille du buffer de reception
-	char buff_recv[dim_buff_recv];
-	string requete_dateHeure = "GET /timer.php HTTP/1.1\r\nHost: 10.187.120.33\r\n\r\n";
-
-	// Envoie de la requete au serveur
-	send(ids_client, requete.c_str(), requete.size() + 1, 0);
-
-	// Réception de la reponse du serveur
-	recv(ids_client, buff_recv, sizeof(buff_recv), 0);
-}
-*/
