a8cc052659a28ea7205439d89531e58a95298ece
Sand-jrd <47563242+Sand-jrd@users.noreply.github.com>
2021-01-02 09:36:51 +0100

    Update do_the_evals.sh

--- do_the_evals.sh
+++ do_the_evals.sh
@@ -4,7 +4,7 @@
 while IFS= read -r line; do
   sleep 0.015
   start="$(date -u +%s.%N)"
-  ./linux-sgx/linux/installer/bin/here/sgxsdk/SampleCode/SampleEnclave/app "${line::-1}" >> output
+  ./app "${line::-1}" >> output
   printf "\n" >> output
   end="$(date -u +%s.%N)"
   echo  "$(bc<<<"$end - $start")" >> benchmaks
